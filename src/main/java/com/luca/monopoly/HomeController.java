9770381bd30c2c59931d38ae9dda0bdd13c2158d
lukeharley <51681749+lukeharley@users.noreply.github.com>
2023-11-20 23:58:34 +0100

    Aggiunti segnalini e portafoglio in home.html

--- src/main/java/com/luca/monopoly/HomeController.java
+++ src/main/java/com/luca/monopoly/HomeController.java
@@ -1,6 +1,11 @@
 package com.luca.monopoly;
 
+import com.luca.monopoly.domain.Giocatore;
 import com.luca.monopoly.domain.Monopoly;
+import com.luca.monopoly.domain.Segnalini;
+
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,18 +17,27 @@ public class HomeController {
     @GetMapping("/")
     public String getHome(Model model) {
 
-        model.addAttribute("dadi", new Monopoly().getDadi().size());
         model.addAttribute("form", new HomeForm());
+        model.addAttribute("dadi", new Monopoly().getDadi().size());
+        model.addAttribute("portafoglioPrimoGiocatoreAggiunto",
+                new Giocatore(Segnalini.CANE, 1500, 0).getPortafoglio());
+
+        List<Segnalini> segnalini = new Monopoly().getSegnalini();
+        model.addAttribute("segnalini", segnalini);
 
         return "home";
+
     }
 
     @PostMapping("/")
     public String aggiungiGiocatore(Model model, HomeForm form) {
 
-        model.addAttribute("dadi", new Monopoly().getDadi().size());
         model.addAttribute("form", form);
+        model.addAttribute("dadi", new Monopoly().getDadi().size());
         model.addAttribute("nomeGiocatoreAggiunto", form.getNome());
+        model.addAttribute("segnalinoScelto", form.getSegnalino());
+        model.addAttribute("portafoglioPrimoGiocatoreAggiunto",
+                new Giocatore(Segnalini.CANE, 1500, 0).getPortafoglio());
 
         return "home";
     }
--- src/main/java/com/luca/monopoly/HomeForm.java
+++ src/main/java/com/luca/monopoly/HomeForm.java
@@ -1,8 +1,12 @@
 package com.luca.monopoly;
 
+import com.luca.monopoly.domain.Segnalini;
+
 public class HomeForm {
 
     private String nome;
+    private int portafoglio;
+    private Segnalini segnalino;
 
     public String getNome() {
         return nome;
@@ -11,4 +15,21 @@ public class HomeForm {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public int getPortafoglio() {
+        return portafoglio;
+    }
+
+    public void setPortafoglio(int portafoglio) {
+        this.portafoglio = portafoglio;
+    }
+
+    public Segnalini getSegnalino() {
+        return segnalino;
+    }
+
+    public void setSegnalino(Segnalini segnalino) {
+        this.segnalino = segnalino;
+    }
+
 }
